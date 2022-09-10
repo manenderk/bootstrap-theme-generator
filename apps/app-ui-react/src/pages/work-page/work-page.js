4f64eef6b000b2a5b9bea55b7e9b03a0e0680316
Manender <manender.kumar@mamsys.com>
2022-09-10 16:47:38 +0530

    added bootstrap components preview

--- apps/app-ui-react/src/pages/work-page/work-page.js
+++ apps/app-ui-react/src/pages/work-page/work-page.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
+import PreviewResultComponent from '../../components/preview-result-component/preview-result-component';
 import './work-page.scss';
 export function WorkPage(props) {
-
   const [sections, setSections] = useState([]);
 
   useEffect(() => {
@@ -10,37 +10,44 @@ export function WorkPage(props) {
       arr.push(i);
     }
     setSections(arr);
-  }, [])
+  }, []);
 
   return (
     <div className="container-fluid">
       <div className="row work-page">
         <div className="col-md-2 column bg-light">
           <ul className="nav flex-column">
-            {
-              sections.map((s, i) => (
-                <li className="nav-item my-1 rounded bg-white" key={i}>
-                  <a className="nav-link text-secondary" aria-current="page" href={`#section-${s}`}>Section {s}</a>
-                </li>
-              ))
-            }
+            {sections.map((s, i) => (
+              <li className="nav-item my-1 rounded bg-white" key={i}>
+                <a
+                  className="nav-link text-secondary"
+                  aria-current="page"
+                  href={`#section-${s}`}
+                >
+                  Section {s}
+                </a>
+              </li>
+            ))}
           </ul>
-
         </div>
         <div className="col-md-5 column">
-          {
-            sections.map((s, i) => (
-              <div className="my-5 py-5" key={i} id={`section-${s}`}>
-                <h1 className="display-1">Section {s}</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit fugit dolor earum veniam blanditiis, illo ullam amet totam harum laudantium voluptatibus. Commodi odio, ipsum asperiores magnam assumenda hic ea? Totam!</p>
-              </div>
-            ))
-          }
+          {sections.map((s, i) => (
+            <div className="my-5 py-5" key={i} id={`section-${s}`}>
+              <h1 className="display-1">Section {s}</h1>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Impedit fugit dolor earum veniam blanditiis, illo ullam amet
+                totam harum laudantium voluptatibus. Commodi odio, ipsum
+                asperiores magnam assumenda hic ea? Totam!
+              </p>
+            </div>
+          ))}
         </div>
-        <div className="col-md-5 column"></div>
+        <div className="col-md-5 column">
+          <PreviewResultComponent />
+        </div>
       </div>
     </div>
-
   );
 }
 export default WorkPage;
